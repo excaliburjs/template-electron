4484276b2dafd61e13b6c72560ae152dd1f0138a
Kamran Ayub <kamran.ayub@gmail.com>
2017-12-22 14:52:30 -0600

    Get build dists working

--- game/index.ts
+++ game/index.ts
@@ -5,7 +5,7 @@ let x = new Greeter("Welcome to Excalibur");
 alert(x.greet());
 
 var game = new ex.Engine({ displayMode: ex.DisplayMode.FullScreen });
-var hello = new ex.Label('Hello Electron', game.getDrawWidth() / 2, 100, 'Segoe UI Light');
+var hello = new ex.Label('Hello Electron', game.canvasWidth / 2, 100, 'Segoe UI Light');
 hello.color = ex.Color.White;
 hello.fontSize = 50;
 hello.textAlign = ex.TextAlign.Center;
