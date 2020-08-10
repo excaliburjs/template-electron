7493619d2c49f34522b4b0734baa96498bc2d5af
VitalyTartynov <Vitaly.Tartynov@GMail.com>
2020-08-10 18:28:04 +0200

    update electron-debug.

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const electron = require('electron');
 const app = electron.app;
 
 // adds debug features like hotkeys for triggering dev tools and reload
-require('electron-debug')();
+const debug = require('electron-debug');
+debug({showDevTools: false});
 
 // prevent window being garbage collected
 let mainWindow;
