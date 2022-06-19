6fccf71ddffba0aa10acaf5fdab0935fcab067a1
Erik Onarheim <erik.onarheim@gmail.com>
2022-06-18 22:00:09 -0500

    Write out actual as base64

--- test.spec.js
+++ test.spec.js
@@ -1,23 +1,31 @@
 const { _electron: electron } = require('playwright')
 const { test, expect } = require('@playwright/test')
+const fs = require('fs');
+const { PNG } = require('pngjs');
 
 test('example test', async () => {
-  const electronApp = await electron.launch({ args: ['.'] })
-  const isPackaged = await electronApp.evaluate(async ({ app }) => {
-    // This runs in Electron's main process, parameter here is always
-    // the result of the require('electron') in the main app script.
-    return app.isPackaged;
-  });
+    const electronApp = await electron.launch({ args: ['.'] })
+    const isPackaged = await electronApp.evaluate(async ({ app }) => {
+        // This runs in Electron's main process, parameter here is always
+        // the result of the require('electron') in the main app script.
+        return app.isPackaged;
+    });
 
-  expect(isPackaged).toBe(false);
+    expect(isPackaged).toBe(false);
 
-  // Wait for the first BrowserWindow to open
-  // and return its Page object
-  const window = await electronApp.firstWindow();
-//   await window.screenshot({ path: 'actual-start.png' });
+    // Wait for the first BrowserWindow to open
+    // and return its Page object
+    const window = await electronApp.firstWindow();
 
-  await expect(window).toHaveScreenshot('expected-start.png');
+    await expect(window).toHaveScreenshot('expected-start.png');
+    await window.screenshot({ path: 'actual.png' });
 
-  // close app
-  await electronApp.close();
+    if (fs.existsSync('./actual.png')) {
+        const actualImage = PNG.sync.read(fs.readFileSync('./actual.png'));
+
+        console.log('Actual image', 'data:image/png;base64,' + actualImage.data.toString('base64'));
+    }
+
+    // close app
+    await electronApp.close();
 });
